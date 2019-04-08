79e04498a3e33e0a93d211731f099a49833d479e
Khizer Younas <m.khizeryounas@gmail.com>
2019-04-09 01:37:45 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.post('/', function(req, res, next) {
+  res.json(req.body);
+});
 
 module.exports = router;
