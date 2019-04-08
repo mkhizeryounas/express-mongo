4782749a543998cd0bee0308009069ede8f07bea
Khizer Younas <m.khizeryounas@gmail.com>
2019-04-09 01:44:00 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 router.post('/', function(req, res, next) {
+  console.log(req.body)
   res.json(req.body);
 });
 
