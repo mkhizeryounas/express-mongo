442ef62d067688258ff8822c0d53eed821666454
Khizer Younas <m.khizeryounas@gmail.com>
2020-04-24 06:15:17 +0500

    feat: mongoose config changed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
-var mongoose = require("mongoose");
-var keys = require("./keys");
+var mongoose = require('mongoose');
+var keys = require('./keys');
 
-mongoose.connect(keys.mongodb, { useNewUrlParser: true });
+mongoose.connect(keys.mongodb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 module.exports = mongoose.connection;
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') require('dotenv').config();
 module.exports = {
-  mongodb: process.env.mongodb || 'mongodb://localhost:27017/syncr',
+  mongodb: process.env.mongodb || 'mongodb://localhost:27017/webrtc',
   secret: process.env.secret || 'c6aSsUzQBACrdWoWy6g7BkuxwKfkPbmB',
 };
