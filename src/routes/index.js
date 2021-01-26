5c4f81a3a0fe0375771bd24e1a5240e963689495
Khizer Younas <m.khizeryounas@gmail.com>
2021-01-27 03:49:08 +0500

    feat: service name identifier added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { name } from '../../package.json';
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-  res.reply({ data: { about: 'service is working fine' } });
+  res.reply({ data: { about: `${name} service is working fine` } });
 });
 
 module.exports = router;
