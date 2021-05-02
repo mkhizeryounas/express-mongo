7bb774c595bf4df1c4e975b165421449b021b4f5
Khizer Younas <m.khizeryounas@gmail.com>
2021-05-02 07:07:47 +0500

    feat: format updated

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src', 'public')));
