8f2841b7dd1193d4513bdc9e0bc0145c1d11a91e
Khizer Younas <m.khizeryounas@gmail.com>
2025-04-13 12:27:01 -0700

    feat: updating dependencies

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,6 @@ var { MONGO_CONNECTION_STRING } = require('./keys');
 mongoose.connect(MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
