cd07332bdc100ec2c726ccf50e773a882fb4ba5f
Khizer Younas <m.khizeryounas@gmail.com>
2025-04-15 16:22:11 -0700

    feat: middleware updated

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.set('DB', db);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('trust proxy', 1);
 app.use(adminJs.options.rootPath, adminRouter);
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
