00a57e5b6a89934bc93024db94653f2b59abf21c
Khizer Younas <m.khizeryounas@gmail.com>
2025-04-15 09:14:57 -0700

    feat: middleware updated

--- src/admin.js
+++ src/admin.js
@@ -29,7 +29,7 @@ const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
   adminJs,
   {
     authenticate: async (email, password) => {
-      const user = await User.findOne({ email, password });
+      const user = await User.findOne({ email, password, scope: 'ADMIN' });
       if (user) {
         return user;
       }
